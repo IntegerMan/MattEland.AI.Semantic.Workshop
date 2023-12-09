b4342082e4e023ca3bb5cb9b4846ba35a1aa4cb4
Matt Eland <matt.eland@gmail.com>
2023-12-08 22:16:23 -0500

    Added beginnings of the solution

---
+++ MattEland.AI.Semantic.Workshop.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Spectre.Console;
+using System.Text;
+
+// Using UTF8 allows more capabilities for Spectre.Console.
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
+
+string welcomeMarkdown = $"Your machine is configured and ready to go.{Environment.NewLine}{Environment.NewLine}" +
+    "Make sure you have an active Azure account you can log into and use to provision AI resources.";
+
+AnsiConsole.Write(new Panel(welcomeMarkdown)
+    .Header(" Welcome to the Semantic AI Workshop ")
+    .BorderStyle(new Style(Color.Green))
+    .Expand());
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # MattEland.AI.Semantic.Workshop
 Workshop content for CodeMash 2024
+
+## Setups & Prerequisites
+
+- .NET 8 SDK
+- A .NET 8 Capable IDE:
+	- Visual Studio v17.8.0 or higher
+	- VS Code
+- An Azure Account that can use:
+	- Azure AI Services (Speech, Vision, Language)
+	- Azure OpenAI (Note: an OpenAI Account will work instead for Parts 2 & 3 of this Workshop)
+
+- You have cloned the https://github.com/IntegerMan/MattEland.AI.Semantic.Workshop git repository locally
+
+- You’ve run the `ConsoleApp` project and verified you see the “Ready to go!” message
