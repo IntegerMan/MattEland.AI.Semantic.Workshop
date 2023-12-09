205791f511a11f3cb278871314d8fcb584645716
Matt Eland <matt.eland@gmail.com>
2023-12-08 22:30:32 -0500

    Added TODO

--- MattEland.AI.Semantic.Workshop.ConsoleApp/Program.cs
+++ MattEland.AI.Semantic.Workshop.ConsoleApp/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 Console.OutputEncoding = Encoding.UTF8;
 Console.InputEncoding = Encoding.UTF8;
 
+// TODO: Verify the presence of Environment Variables
+
 string welcomeMarkdown = $"Your machine is configured and ready to go.{Environment.NewLine}{Environment.NewLine}" +
     "Make sure you have an active Azure account you can log into and use to provision AI resources.";
 
